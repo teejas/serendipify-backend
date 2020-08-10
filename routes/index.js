cc59291d21b988f86c0c5f03633246ccbddb34bb
siripur2 <siripur2@illinois.edu>
2020-08-10 15:42:39 -0700

    sample landing page added

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'JUKO // Sir Teej' });
 });
 
 module.exports = router;
