f21bc28f2f4a91f76af53416c0bddac70ea1536a
siripur2 <siripur2@illinois.edu>
2020-08-15 15:37:20 -0700

    made auth_request accept redirect_uri as param

--- api/login.js
+++ api/login.js
@@ -5,19 +5,34 @@ const handleError = require('../utils/handleError.js');
 
 const router = express.Router();
 
-/* GET auth_request */
-router.get('/auth_request', function(req, res, next) {
-  const scopes = 'playlist-read-private playlist-modify-public playlist-modify-private user-library-read user-library-modify user-top-read streaming user-read-playback-state user-modify-playback-state';
+/* POST auth_request: body contains redirect_uri */
+router.post('/auth_request', function(req, res, next) {
+  try {
+    var redirect_uri = req.body.redirect_uri;
 
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "*");
+    console.log(redirect_uri)
 
-  res.status(200).json('https://accounts.spotify.com/authorize' +
-    '?response_type=code' +
-    '&client_id=' + spotifyCredentials.CLIENT_ID +
-    (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
-    '&redirect_uri=' + encodeURIComponent(spotifyCredentials.REDIRECT_URI)
-  );
+    if(redirect_uri) {
+
+      const scopes = 'playlist-read-private playlist-modify-public playlist-modify-private user-library-read user-library-modify user-top-read streaming user-read-playback-state user-modify-playback-state';
+
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "*");
+
+      res.status(200).json('https://accounts.spotify.com/authorize' +
+        '?response_type=code' +
+        '&client_id=' + spotifyCredentials.CLIENT_ID +
+        (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
+        '&redirect_uri=' + encodeURIComponent(redirect_uri)
+      );
+    } else {
+      const msg = "No redirect uri for /auth_request...";
+      handleError(res, msg, msg, 500);
+    }
+  } catch(error) {
+    const msg = "Error with /auth_request endpoint...";
+    handleError(res, error, msg, 500);
+  }
 })
 
 /* POST get_tokens: body contains auth_code */
@@ -37,13 +52,13 @@ router.post('/get_tokens', function(req, res, next) {
       }
     }
     axios.post('https://accounts.spotify.com/api/token', body, config)
-    .then((tokens_res) => {
-      console.log('Body: ', tokens_res.data);
-      res.status(200).json(tokens_res.data);
-    }).catch((err) => {
-      const msg = "Error with POST request to spotify API for /get_tokens...";
-      handleError(res, error, msg, 500);
-    });
+      .then((tokens_res) => {
+        console.log('Body: ', tokens_res.data);
+        res.status(200).json(tokens_res.data);
+      }).catch((err) => {
+        const msg = "Error with POST request to spotify API for /get_tokens...";
+        handleError(res, error, msg, 500);
+      });
   } catch(error) {
     const msg = "Error with /get_tokens endpoint...";
     handleError(res, error, msg, 500);
